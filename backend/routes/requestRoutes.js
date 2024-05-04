b1ad88130c434d5447fc5a5dbc30456ba6f73c8d
Jose Eduardo <00408048@anahuacmayab.edu.mx>
2024-05-03 18:40:34 -0600

    first commit

--- backend/routes/requestRoutes.js
+++ backend/routes/requestRoutes.js
@@ -2,3 +2,9 @@ const express = require('express')
 const router = express.Router()
 const {protect} = require('../middleware/authMiddleware')
 
+const {getRequest, crearRequest, updateRequest, deleteRequest} = require('../controllers/requestController')
+
+router.route('/').get(protect, getRequest).post(protect, crearRequest)
+router.route('/:id').delete(protect, deleteRequest).put(protect, updateRequest)
+
+module.exports = router
